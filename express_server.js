1d6b6982733881aebfe4f634d93a17841c09adf2
Woncheol Kim <icijiy@gmail.com>
2022-08-08 23:35:14 -0700

    adding route and header

--- express_server.js
+++ express_server.js
@@ -25,6 +25,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/b2xVn2", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
