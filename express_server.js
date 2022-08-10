69014d37c888bf11954e148cf645f1d9d4d44499
Woncheol Kim <icijiy@gmail.com>
2022-08-10 01:10:21 -0700

    added cookies

--- express_server.js
+++ express_server.js
@@ -1,10 +1,23 @@
-const { response } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser");
 
+app.use(cookieParser());
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }));
+
+
+function generateRandomString() {
+  var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  var result = ""
+  var charactersLength = characters.length;
+  
+  for ( var i = 0; i < 5 ; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+  }
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,28 +28,37 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  console.log(templateVars)
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase,
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase,
+  };
+  res.render("urls_new", templateVars);
 });
+
 //redirect url details and save
-app.get('/urls/:id', (req, res) => {
-  const templateVar = { id: req.params.id, longURL: urlDatabase[req.params.id]};
-  res.render('urls_show', templateVar);
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"],
+    urls: urlDatabase,
+  };
+  res.render("urls_show", templateVars);
 });
+
 //save and go to website
 app.get('/u/:id', (req, res) => {
   const templateVar = { id: req.params.id, longURL: urlDatabase[req.params.id]};
@@ -46,15 +68,33 @@ app.get('/u/:id', (req, res) => {
   res.redirect('/418');
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   const id = generateRandomString();
   urlDatabase [id] = req.body.longURL
   res.redirect(`/u/${id}`)
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect(`/urls`);
+});
+
+app.post("/urls/:id/update", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  const username = req.body.username;
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id", (req, res) => {
@@ -68,13 +108,7 @@ app.post("/urls/:id", (req, res) => {
   // res.redirect(`/u/${id}`)
 });
 
-function generateRandomString() {
-var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-var result = ""
-var charactersLength = characters.length;
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
-for ( var i = 0; i < 5 ; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-}
-return result;
-}
